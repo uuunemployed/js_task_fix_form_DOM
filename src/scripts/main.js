5cc1538588b73cd832f59677289ad869f8de50ec
Ivan Denys <denisivan06007@gmail.com>
2025-07-30 13:53:48 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,15 @@ const inputs = Array.from(document.querySelectorAll('input'));
 for (const input of inputs) {
   const label = document.createElement('label');
 
-  let inputName = input.name[0].toUpperCase();
+  if (input.name.length > 0) {
+    let inputName = input.name[0].toUpperCase();
 
-  inputName += input.name.slice(1);
+    inputName += input.name.slice(1);
+    input.before(label);
+    input.setAttribute('placeholder', inputName);
 
-  label.setAttribute('for', input.id);
-  label.textContent = inputName;
-  label.classList.add('field-label');
-  input.before(label);
-  input.setAttribute('placeholder', inputName);
+    label.setAttribute('for', input.id);
+    label.textContent = inputName;
+    label.classList.add('field-label');
+  }
 }
